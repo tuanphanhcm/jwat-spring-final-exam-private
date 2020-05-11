5961e89fda5a9455ed4d8f87decf689d1e1ecfec
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2020-05-11 10:17:30 +0700

    Init submit

---
+++ src/main/java/vn/com/cyberlogitec/jwat/JwatSpringFinalExamApplication.java
@@ -0,0 +1,13 @@
+package vn.com.cyberlogitec.jwat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwatSpringFinalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwatSpringFinalExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/cyberlogitec/jwat/JwatSpringFinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.cyberlogitec.jwat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwatSpringFinalExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
