779fb4738dba0673687ae13b97ad6e6b4967c508
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2020-05-19 10:46:59 +0700

    final exam

--- src/main/java/vn/com/cyberlogitec/jwat/endpoint/EndPoint.java
+++ src/main/java/vn/com/cyberlogitec/jwat/endpoint/EndPoint.java
@@ -1,22 +1,11 @@
 package vn.com.cyberlogitec.jwat.endpoint;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import vn.com.cyberlogitec.jwat.model.Ship;
-import vn.com.cyberlogitec.jwat.model.Shipment;
-import vn.com.cyberlogitec.jwat.model.Voyage;
-import vn.com.cyberlogitec.jwat.model.dto.VoyageShipmentDto;
-import vn.com.cyberlogitec.jwat.service.ShipService;
-import vn.com.cyberlogitec.jwat.service.ShipmentService;
-import vn.com.cyberlogitec.jwat.service.VoyageService;
 
 @RestController
 @RequestMapping("/api/endpoint")
